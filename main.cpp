1ee86d32c54550c0ddadd1466cc745f1c776e65d
Nikita Krenov <nikitabaram@gmail.com>
2024-06-06 19:15:50 +0300

    now all great

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
-template <typename type>
+int find(int base, int exponent) {
+	if (exponent == 0) {
+		return 1;
+	}
+	else if (exponent < 0) {
+		return 1 / find(base, -exponent);
+	}
+	else {
+		return base * find(base, exponent - 1);
+	}
+}
+
 
 int main() {
+	int base, exponent;
+	cout << "Enter the base: ";
+	cin >> base;
+	cout << "Enter the exponent: ";
+	cin >> exponent;
+
+	int result = find(base, exponent);
+	cout << base << " raised to the power of " << exponent << " is: " << result << endl;
 
 	system("pause");
 	return 0;
