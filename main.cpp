57e2ef1d06c944650bd64994d8e2d5ba18445f2c
Nikita Krenov <nikitabaram@gmail.com>
2024-06-06 19:22:06 +0300

    user enter a number and program has a print stars a line

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
+void stars(int n) {
+	if (n >= 0) {
+		cout << "*";
+		stars(n - 1);
+	}
+}
+
 int main() {
+	int n;
+	cout << "Enter a number: ";
+	cin >> n;
+	stars(n);
+	cout << endl;
 
 	system("pause");
 	return 0;
