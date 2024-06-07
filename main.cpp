289990548c7e6e3dde1ecd39ddcbc39fcb6355c3
Nikita Krenov <nikitabaram@gmail.com>
2024-06-07 13:00:41 +0300

    Write a recursive function for finding the greatest common divisor of two integers

--- main.cpp
+++ main.cpp
@@ -7,4 +7,23 @@ int main() {
 
 	system("pause");
 	return 0;
+    #include <iostream>
+
+    using namespace std;
+
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    int main() {
+        int num1, num2;
+        cout << "Enter two numbers: ";
+        cin >> num1 >> num2;
+        int result = gcd(num1, num2);
+        cout << "Greatest Common Divisor: " << result << endl;
+        return 0;
+    }
 }
